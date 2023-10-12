1a817f1015c53ead014d80817588547b95f310c1
masechkacat <masechkacat@gmail.com>
2023-10-12 16:19:01 +0200

    dark-trader

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -1,4 +1,21 @@
-#test
-#test
-#test
-#test
+require 'nokogiri'
+require 'open-uri'
+
+def scrape_crypto_data
+  url = 'https://coinmarketcap.com/all/views/all/'
+  doc = Nokogiri::HTML(URI.open(url))
+  crypto_data = []
+
+  rows = doc.css('tr.cmc-table-row')
+
+  rows.each do |row|
+    symbol = row.css('td.cmc-table__cell.cmc-table__cell--sort-by__symbol').text
+    price = row.css('td.cmc-table__cell.cmc-table__cell--sort-by__price span').text.gsub('$', '').to_f
+
+    crypto_data << { symbol => price } unless symbol.empty?
+  end
+
+  crypto_data
+end
+
+puts scrape_crypto_data
